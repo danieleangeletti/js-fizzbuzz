e65695468c5b8c98f4a7249a37de8fd998dad9f1
danieleangeletti <angelettidaniele@gmail.com>
2023-12-05 15:47:18 +0100

    esercizio completato

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -19,5 +19,19 @@ fizz = "Fizz";
 buzz = "Buzz";
 
 for (let i = 1; i < 101; i++ ) {
-    console.log(i);
+    if ((i % 3 == 0) && (i % 5 != 0)) {
+        console.log(fizz);
+    }
+
+    else if ((i % 5 == 0) && (i % 3 != 0)) {
+        console.log(buzz);
+    }
+
+    else if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log(fizz + buzz);
+    }
+
+    else {
+        console.log(i);
+    }
 }
